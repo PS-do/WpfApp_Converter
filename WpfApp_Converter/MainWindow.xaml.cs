ca0d3614932d195624342a1beea4b43cd88ada08
Сергей Плюхин <psg.pgs@mail.ru>
2022-12-21 17:49:58 +0300

    Edit

--- WpfApp_Converter/MainWindow.xaml.cs
+++ WpfApp_Converter/MainWindow.xaml.cs
@@ -29,6 +29,10 @@ namespace WpfApp_Converter
         {
             return Convert.ToString(Convert.ToDouble(rate) * Convert.ToDouble(sum));
         }
+        string ConvertL(string L, double k)
+        {
+            return Convert.ToString(Convert.ToDouble(L) * k);
+        }
 
         private void Button_d_Click(object sender, RoutedEventArgs e)
         {    
@@ -45,6 +49,23 @@ namespace WpfApp_Converter
         private void Button_dr_Click(object sender, RoutedEventArgs e)
         {
             dr_resSum.Text = ConvertValut(dr_rate.Text, dr_sum.Text);
+        }
+
+        private void Button_inch_Click(object sender, RoutedEventArgs e)
+        {
+            inch_res.Text = ConvertL(inch_L.Text, 0.0254);
+        }
+        private void Button_foot_Click(object sender, RoutedEventArgs e)
+        {
+            foot_res.Text = ConvertL(foot_L.Text, 0.3048);
+        }
+        private void Button_mile_Click(object sender, RoutedEventArgs e)
+        {
+            mile_res.Text = ConvertL(mile_L.Text, 1609.0);
+        }
+        private void Button_verst_Click(object sender, RoutedEventArgs e)
+        {
+            verst_res.Text = ConvertL(verst_L.Text, 1066.8);
         }
     }
 }
